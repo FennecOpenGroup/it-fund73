61b3b838d85c142755e8914c53b39fc24d21723a
SerJPogareli <Patrix15@yandex.ru>
2023-07-17 04:23:58 +0300

    CalendarModalEvent

--- src/components/modals/ModalCalendarEventInfo.tsx
+++ src/components/modals/ModalCalendarEventInfo.tsx
@@ -6,17 +6,21 @@ import {
   ModalContent,
   ModalHeader,
   ModalOverlay,
-  useMediaQuery,
+  VStack,
+  Text,
+  HStack,
 } from '@chakra-ui/react';
 import React, { Dispatch, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import ReactMarkdown from 'react-markdown';
 import 'react-phone-input-2/lib/style.css';
+import { MdDateRange } from 'react-icons/md';
+import { FaMapMarkerAlt } from 'react-icons/fa';
 
 import { coreRemoveVisibleModal } from '../../actions/coreActions';
 import { RootActions } from '../../types/RootActions';
 import { ModalsEnum } from '../../enums/ModalsEnum';
 import { IRootState } from '../../interfaces/IRootState';
-import { useWindowDimensions } from '../../hooks/useWindowDimensions';
 
 interface IModalCalendarNewDateProps {
   isOpen: boolean;
@@ -30,24 +34,42 @@ export const ModalCalendarEventInfo = React.memo(({ isOpen, id }: IModalCalendar
   const themeIsDark = useSelector((state: IRootState) => state.core.themeIsDark);
   const events = useSelector((state: IRootState) => state.core.events);
 
-  const { width } = useWindowDimensions();
-
-  const [isLargerThan1000] = useMediaQuery('(min-width: 800px)');
-  events && console.log(events[Number(id)].attributes.name);
   return (
     <>
       <Modal isOpen={isOpen} onClose={handleClose} size="md">
         <ModalOverlay />
-        <ModalContent
-          bg={themeIsDark ? '#121212' : 'white'}
-          border="2px"
-          minW={isLargerThan1000 ? width / 4.5 : width / 1.8}
-        >
+        <ModalContent bg={themeIsDark ? '#121212' : 'white'} border="2px">
           <ModalCloseButton color={themeIsDark ? 'white' : 'brand.dark'} />
           <ModalHeader fontSize={['lg', 'xl']} color={themeIsDark ? 'white' : 'brand.dark'}>
-            Данные мероприятия
+            {events && events[Number(id)].attributes.name}
           </ModalHeader>
-          <ModalBody w="full"></ModalBody>
+          <ModalBody>
+            <VStack w="full" align="start">
+              <Text color={themeIsDark ? 'white' : 'brand.dark'} fontSize={['sm', 'md']}>
+                {events && <ReactMarkdown>{events[Number(id)].attributes.text}</ReactMarkdown>}
+              </Text>
+            </VStack>
+            <VStack w="full" align="start" spacing={0} pt={4}>
+              <HStack>
+                <MdDateRange size="1.2em" color="gray" />
+                <Text color="gray" fontSize={['sm', 'md']}>
+                  {events &&
+                    new Date(`${events[Number(id)].attributes.date}`).toLocaleDateString('ru-RU', {
+                      weekday: 'long',
+                      year: 'numeric',
+                      month: 'long',
+                      day: 'numeric',
+                    })}
+                </Text>
+              </HStack>
+              <HStack>
+                <FaMapMarkerAlt size="1.2em" color="gray" />
+                <Text color="gray" fontSize={['sm', 'md']}>
+                  {events && events[Number(id)].attributes.address}
+                </Text>
+              </HStack>
+            </VStack>
+          </ModalBody>
         </ModalContent>
       </Modal>
     </>
