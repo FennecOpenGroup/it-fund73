5facdb19a28ad6df0ebc80ab56e940c1f8431875
sIIIDR <kirill.sidorenkov.2003@yandex.ru>
2023-12-21 18:49:22 +0400

    iframe fix

--- src/pages/education/Education.tsx
+++ src/pages/education/Education.tsx
@@ -213,7 +213,7 @@ export const Education = React.memo(() => {
       </VStack>
       <iframe
         src="https://kitchat.kitbot.cloud/navigator/"
-        style={{ width: '100%', height: '100%' }}
+        style={{ width: '100%', height: '100%', minHeight: '100vh' }}
       ></iframe>
       <VStack ref={refFooter}>
         <Footer />
