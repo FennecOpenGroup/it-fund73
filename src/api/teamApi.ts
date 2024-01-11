cc373b38f6cb96b5f51426648e4f2132aadfdd75
root <root@DESKTOP-FP2S1V5.localdomain>
2024-01-11 08:00:51 +0400

    Change API url

--- src/api/teamApi.ts
+++ src/api/teamApi.ts
@@ -11,3 +11,4 @@ export async function fetchTeam(): Promise<IImage[]> {
     headers: { 'Content-Type': 'application/json' },
   }).then(handleResponse);
 }
+  
--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,3 +1,3 @@
-export const API_URL_ADMIN = 'https://admin.айтифонд.рус/';
+export const API_URL_ADMIN = 'https://admin.айтифонд.рус';
 export const API_URL = 'https://айтифонд.рус';
     
